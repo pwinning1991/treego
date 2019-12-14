3feb82f11735bba7f55afb133ddb597b026cc273
Philip Winnington <ftcpjw@yahoo.com>
2019-12-14 14:58:21 -0500

    adding tests

--- main_test.go
+++ main_test.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-	"log"
 	"testing"
 )
 
 func TestTree(t *testing.T) {
-	result := tree(".", "")
-	got := result
-	if got != nil {
-		log.Fatalf("Did not exit without an error wanted: %v, got %v", nil, got)
-	}
+	t.Run("check valid path", func(t *testing.T) {
+		result := tree(".", "")
+		got := result
+		if got != nil {
+			t.Errorf("Did not exit without an error wanted: %v, got %v", nil, got)
+		}
 
+	})
+
+	t.Run("Check error on path that doesn't exist", func(t *testing.T) {
+		got := tree("path that doesn't exist", "")
+		if got == nil {
+			t.Errorf("did not exit with incorrect path, got: %q, want: %v", got, nil)
+		}
+
+	})
 }
