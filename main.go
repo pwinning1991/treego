f60f012aa78b1034aa29a83f183b225fd55e849d
Philip Winnington <ftcpjw@yahoo.com>
2019-12-14 11:57:03 -0500

    inital attempt

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -11,6 +14,31 @@ func main() {
 		args = os.Args[1:]
 	}
 
-	fmt.Println(args)
+	for _, arg := range args {
+		err := tree(arg)
+		if err != nil {
+			log.Printf("tree %s: %v\n", arg, err)
+		}
+	}
+}
 
+func tree(root string) error {
+	err := filepath.Walk(root, func(path string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+		if fi.Name()[0] == '.' {
+			return filepath.SkipDir
+		}
+
+		rel, err := filepath.Rel(root, path)
+		if err != nil {
+			return fmt.Errorf("could not rel(%s, %s): %v", root, path, err)
+		}
+		depth := len(strings.Split(rel, string(filepath.Separator)))
+		fmt.Printf("%s%s\n", strings.Repeat(" ", depth), fi.Name())
+
+		return nil
+	})
+	return err
 }
